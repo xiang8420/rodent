6e326c3dfe347882c5771deb7971b166ab90d0ec
xiang8420 <xuxiang8420@outlook.com>
2020-10-10 10:58:00 +0800

    Remove buffer.h print

--- src/driver/buffer.h
+++ src/driver/buffer.h
@@ -21,15 +21,6 @@ static void decompress(const std::vector<char>& in, Array& out) {
 
 template <typename Array>
 static void read_buffer(std::istream& is, Array& array) {
-//    size_t in_size = 0, out_size = 0;
-//    is.read((char*)&in_size,  sizeof(size_t));
-//    is.read((char*)&out_size, sizeof(size_t));
-//    std::vector<char> in(out_size);
-//    is.read(in.data(), in.size());
-//    array = std::move(Array(in_size / sizeof(array[0])));
-//    decompress(in, array);
-    printf("before read buffer %ld mb\n", physical_memory_used_by_process() / 1024);
-
     size_t size;
     is.read((char*)&size, sizeof(size_t));
 
@@ -37,7 +28,6 @@ static void read_buffer(std::istream& is, Array& array) {
     array = std::move(Array(size / sizeof(array[0])));
     char* out = (char*)array.data();
     size_t st = 0;
-    printf("before decompress Memory %ld \n", physical_memory_used_by_process() / 1024);
     while(st < size) {
         size_t compress_size, decompress_size;
        
@@ -48,12 +38,6 @@ static void read_buffer(std::istream& is, Array& array) {
         in.resize(compress_size); 
         is.read(in.data(), in.size());
         
-        printf("in size %ld \n",compress_size);
-        printf("origin data %ld\n", decompress_size); 
-        for(int i =0; i < 100; i++) {
-            printf("%c ", in[st + i]);
-        }
-        printf("\n");
         LZ4_decompress_safe(in.data(), &out[st], in.size(), decompress_size);
 
         st += decompress_size;
@@ -62,7 +46,6 @@ static void read_buffer(std::istream& is, Array& array) {
 
 template <typename Array>
 static void read_buffer(const std::string& file_name, Array& array) {
-    printf("read %s\n", file_name);
     std::ifstream is(file_name, std::ios::binary);
     read_buffer(is, array);
 }
@@ -83,18 +66,6 @@ static void compress(const Array& in, size_t size, std::vector<char>& out) {
 
 template <typename Array>
 static void write_buffer(std::ostream& os, const Array& array) {
-//    std::vector<char> out;
-//    compress(array, out);
-//    size_t in_size  = sizeof(array[0]) * array.size();
-//    size_t out_size = out.size();
-//    os.write((char*)&in_size,  sizeof(uint32_t));
-//    os.write((char*)&out_size, sizeof(uint32_t));
-//    os.write(out.data(), out.size());
-    printf("before compress Memory %ld \n", physical_memory_used_by_process() / 1024);
-    float* d = (float*)array.data();
-    for(size_t i = 0; i < 10; i ++) 
-        printf("%f ", d[i]);
-    printf("\n");
     char* in = (char*)array.data();
 
     int st = 0;
@@ -102,30 +73,17 @@ static void write_buffer(std::ostream& os, const Array& array) {
     os.write((char*)&size, sizeof(size_t));
 
     size_t lz4_max = LZ4_MAX_INPUT_SIZE;
-    printf("all size %ld lz4 max  %ld\n", size, lz4_max);
     while(st < size) {
         size_t origin_size = size - st > lz4_max ? lz4_max : size - st; 
         printf("before compress Memory %ld array size %ld array[0 ]%ld %d\n", physical_memory_used_by_process() / 1024, array.size(), sizeof(array[0]), LZ4_compressBound(origin_size));
-        printf("origin data %ld\n", origin_size); 
-        for(int i =0; i < 100; i++) {
-            printf("%c ", in[st + i]);
-        }
-        printf("\n\n");
         std::vector<char> out;
         out.resize(LZ4_compressBound(origin_size));
         out.resize(LZ4_compress_default((const char*)&in[st], (char*)out.data(), origin_size, out.size()/*LZ4_compressBound(origin_size)*/));
-//        for(int i =0; i < 100; i++) {
-//            printf("%c ", out[i]);
-//        }
-//        printf("\n");
         
         size_t compress_size = out.size();
 
         os.write((char*)&origin_size, sizeof(size_t));
         os.write((char*)&compress_size, sizeof(size_t));
-        printf("origin size %ld out size%ld\n",origin_size, out.size());
-        printf("after compress Memory %ld %d\n", physical_memory_used_by_process() / 1024, LZ4_compressBound(origin_size));
-    
         os.write(out.data(), out.size());
 
         st += origin_size;
